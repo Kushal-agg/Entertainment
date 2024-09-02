00d35f81e8f45aaf21fa53a47cea93bac23bfe12
Kushal Aggarwal <kushalaggarwal.ka1016@gmail.com>
2024-09-02 13:09:25 +0530

    new commit

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,13 +1,44 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { CiSearch } from "react-icons/ci";
-import "./searchRes.scss"; // Ensure this file contains the necessary styles
+import "./searchRes.scss";
 
 const apiKey = "14af83f372fe18ca097a8721d92b7145";
 const url = "https://api.themoviedb.org/3";
 const imgUrl = "https://image.tmdb.org/t/p/original";
 
-const Cards = ({ img, title, overview, mediaType, releaseDate }) => {
+const KnownFor = ({ items }) => {
+  return (
+    <div className="known-for">
+      <h4>Known For</h4>
+      <div className="known-for-grid">
+        {items.map((item, index) => (
+          <div key={index} className="known-for-item">
+            <img
+              src={`${imgUrl}${item.poster_path}`}
+              alt={item.title || item.name}
+              className="known-for-img"
+            />
+            <p className="known-for-title">{item.title || item.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Cards = ({
+  img,
+  title,
+  overview,
+  mediaType,
+  releaseDate,
+  popularity,
+  voteAverage,
+  language,
+  originCountry,
+  knownFor,
+}) => {
   return (
     <div className="cards">
       <img src={img} alt={title} className="cards-img" />
@@ -18,19 +49,35 @@ const Cards = ({ img, title, overview, mediaType, releaseDate }) => {
         {releaseDate && (
           <p className="cards-release">Release Date: {releaseDate}</p>
         )}
+        {language && <p className="cards-language">Language: {language}</p>}
+        {originCountry && (
+          <p className="cards-country">Origin Country: {originCountry}</p>
+        )}
+        {mediaType === "person" && knownFor && knownFor.length > 0 && (
+          <KnownFor items={knownFor} />
+        )}
+        <p className="cards-popularity">Popularity: {popularity}</p>
+        {voteAverage && <p className="cards-vote">Rating: {voteAverage}/10</p>}
       </div>
     </div>
   );
 };
 
-const Column = ({ title, arr = [] }) => {
+const Column = ({ arr = [] }) => {
   return (
     <div className="column">
-      <h2 className="column-title">{title}</h2>
       <div className="column-content">
         {arr.map((item, index) => {
           const imgPath = item.poster_path || item.profile_path;
           const imgSrc = imgPath ? `${imgUrl}${imgPath}` : null;
+          const knownFor = item.known_for || [];
+          const language = item.original_language
+            ? item.original_language.toUpperCase()
+            : null;
+          const originCountry = item.origin_country
+            ? item.origin_country.join(", ")
+            : null;
+
           return imgSrc ? (
             <Cards
               key={index}
@@ -39,6 +86,11 @@ const Column = ({ title, arr = [] }) => {
               overview={item.overview}
               mediaType={item.media_type}
               releaseDate={item.release_date || item.first_air_date}
+              popularity={item.popularity}
+              voteAverage={item.vote_average}
+              language={language}
+              originCountry={originCountry}
+              knownFor={knownFor}
             />
           ) : null;
         })}
